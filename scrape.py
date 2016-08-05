ae98da1db78cd0096d1657e3b5d4c222a8f6c5f6
Jeremy Kieser <kieserjw@gmail.com>
2016-08-05 16:37:58 -0500

    removed timeout logic

--- scrape.py
+++ scrape.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 import urllib
-import time 
 
 if __name__ == '__main__':
    url = 'http://extrazoom.com/files/gef/image-5418/13/'
    ext = '.jpeg'
-   sleep_time = 10
    
    rows = 14
    cols = 10
@@ -19,6 +17,3 @@ if __name__ == '__main__':
          #save the file to disk
          urllib.urlretrieve(url + file_name, file_name)
          print url + file_name
-         
-         #A lot of requests in a row might cause timeouts
-         time.sleep(sleep_time)
